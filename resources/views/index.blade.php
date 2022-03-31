98363eef1c18187ec7cfb5c4185de25df7fd3aaf
AliceColombari <colombari.23@gmail.com>
2022-03-31 16:50:58 +0200

    stampa - esercizio ok

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,5 +7,20 @@
 @section('content')
 
     {{-- qui ci va il ciclo per stampare tutto il contenuto del db cioè tutti i miei film --}}
-    
+    @foreach ($travels as $travel)
+        <div class="title">
+            <h1>{{$travel->city}} - {{$travel->address}}</h1>
+            <p>{{$travel->description}}</p>
+        </div>
+        <div class="info">
+            <h2>{{$travel->type}}</h2>
+            <ul>
+                <li>{{$travel->departure}}</li>
+                <li>{{$travel->arrivals}}</li>
+                <li>{{$travel->price}}</li>
+                <li>{{$travel->departure_time}}</li>
+                <li>{{$travel->arrivals_time}}</li>
+            </ul>
+        </div>
+    @endforeach
 @endsection

c3032d7bfa897a9745109ad165c17297cd1486e2
AliceColombari <colombari.23@gmail.com>
2022-03-31 16:14:31 +0200

    errore salvataggio dati

---
+++ database/seeds/TravelsTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Travel;
+use Illuminate\Database\Seeder;
+
+class TravelsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // oggetto che rappresenta istanza di travel
+        // nuova istanza
+        $travel = new Travel();
+
+            // inserisco dati compilati richiesti
+            // popolo dati del mio db da compilare
+            $travel->city = 'Parigi';
+            $travel->address = '2 bis Cité Bergère';
+            $travel->description = null;
+            $travel->type = 'Hotel B&B';
+            $travel->departure = 'Mercoledì 20 giugno';
+            $travel->arrivals = 'Giovedì 21 giugno';
+            $travel->price = 12345678;
+            $travel->departure_time = 0;
+            $travel->arrivals_time = 0;
+            
+            //salvo dentro il db i dati aggiunti
+            $travel->save();
+
+    }
+}
